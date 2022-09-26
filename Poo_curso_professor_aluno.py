aafb4f95cd672b78fbe78d633359eee2173358fd
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-09-26 18:13:20 -0300

    Update Poo_curso_professor_aluno.py

--- Poo_curso_professor_aluno.py
+++ Poo_curso_professor_aluno.py
@@ -1,17 +1,16 @@
 class Curso:
-    def __init__(self, codigo, nome, descricao, coordenador, docentes):
+    def __init__(self, codigo, nome, descricao, coordenador):
         self.codigo = codigo
         self.nome = nome
         self.descricao = descricao
-        self.coordenador = professor
-        self.docentes = professor[]
+        self.coordenador = coordenador
+        self.docentes = []
         
-    def definir_coordenador(self, coordenador: Professor):
-        novo_coordenador = coordenador
+    def definir_coordenador(self, coordenador):
+        self.coordenador = coordenador
     
-    def add_docente(self, professor: Professor):
-        novo_professor = professor
-        novo_professo.append(professor[])
+    def add_docente(self, professor):
+        self.docentes.append(professor)
     
     def imprimir(self):
         print(f'O código do curso é: {self.codigo}')
@@ -21,16 +20,16 @@ class Curso:
         print(f'Os professores do curso são: {self.docentes}')
 
 class Professor:
-    def __init__(self, matricula, nome_, cpf, admissao, email, lotacao):
+    def __init__(self, matricula, nome_, cpf, admissao, email):
         self.matricula = matricula
         self.nome_ = nome_
         self.cpf = cpf
         self.admissao = admissao
         self.email = email
-        self.lotacao = curso
+        self.lotacao = None
     
     def set_lotacao(self, curso: Curso):
-        nova_lotacao = curso
+        self.lotacao = curso
     
     def imprimir(self):
         print(f'A matricula do professor é: {self.matricula}')
@@ -41,16 +40,16 @@ class Professor:
         print(f'A lotação do professor: {self.lotacao}')
 
 class Aluno:
-    def __init__(self, matricula_a, nome_a, cpf_a, email_a, cursos):
+    def __init__(self, matricula_a, nome_a, cpf_a, email_a):
         self.matricula_a = matricula_a
         self.nome_a = nome_a
         self.cpf_a = cpf_a
         self.email_a = email_a
-        self.cursos = curso[]
+        self.cursos = list(Curso)
     
-    def matricular(curso: Curso):
+    def matricular(self, curso: Curso):
         self.cursos.append(curso)
-        print(f'Estes são os cursos no qual o aluno está matriculado: {self.cursos[]}')
+        print(f'Estes são os cursos no qual o aluno está matriculado: {self.cursos}')
     
     def imprimir(self):
         print(f'A matricula do aluno é: {self.matricula_a}')
@@ -59,3 +58,8 @@ class Aluno:
         print(f'A admissao do professor: {self.admissao}')
         print(f'O email do aluno: {self.email_a}')
         print(f'Os cursos que o aluno faz são: {self.cursos}')
+
+biologia = Curso(123, 'Biologia', 'Dificil', 'Jorginho')
+jorge = Professor(123132, 'Jorge', 123123123, '123d1s2a3ds', '213123')
+biologia.add_docente(jorge)
+biologia.imprimir()
