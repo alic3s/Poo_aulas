dc053bf35ac25ba89f55b84cdfa833bfa8bf5391
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-03 18:02:11 -0300

    Update Poo_agenda.py

--- Poo_agenda.py
+++ Poo_agenda.py
@@ -53,3 +53,39 @@ dicio = {'1)': 'Inserir Contato', '2)': 'Pesquisar Contatos', '3)': 'Alterar Con
 
 for k, v in dicio.items():
     print(f'{(k)} {v:^5}')
+
+  
+print ("""
+    1.Inserir Contato
+    2.Pesquisar Contato
+    3.Alterar Contato
+    4.Sair
+    """)
+
+menu = True
+while menu:
+    menu = int(input('Escolha uma opção: '))
+if menu == 1:
+    contato1 = Contato()
+    agenda.add.contato
+elif menu == 2:
+    agenda.pesquisar_nome
+    print(nome.self.contato)
+elif menu == 3:
+    print ("""
+    3.1.Nome
+    3.2.Email
+    3.3.Telefone
+    """)
+    alterar = (float(input('Qual informação deseja alterar?: ')))
+    if alterar == 3.1:
+        contato.alterar_nome
+        print(self.nome)
+    elif alterar == 3.2:
+        contato.alterar_email
+        print(self.email)
+    elif alterar == 3.3:
+        contato.alterar_telefone
+        print(self.telefone)
+elif menu == 4:
+    break
