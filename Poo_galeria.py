e31b7582acaa058e93de8de2abcfb4388c4c8c8c
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-17 20:07:10 -0300

    Update Poo_galeria.py

--- Poo_galeria.py
+++ Poo_galeria.py
@@ -1,1 +1,12 @@
 #socorro essa questao é muito grande e eu não vou saber fazer sozinha
+class Artistas:
+    def __init__(self, nome, codigo, nacionalidade):
+        self._nome = nome
+        self._codigo = codigo
+        self._nacionalidade = nacionalidade
+    
+    def imprimir(self):
+        return f'Nome: {self.nome}\nCodigo: {self._codigo}\nNacionalidade: {self._nacionalidade}'
+
+class Obra:
+    def __init__(self, )
