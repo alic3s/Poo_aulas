e5dae798a4f4dc39ae81b29e791f8e17c00a16e4
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-21 12:36:53 -0300

    Update Poo_VendaIngressos.py

--- Poo_VendaIngressos.py
+++ Poo_VendaIngressos.py
@@ -1,14 +1,17 @@
 '''Implemente as classes: Ingressos, Camarote, Show.
+
 A classe Ingresso é composta por um código único, que deve ser gerado por um
 método estático definido por você ao instanciar o objeto, um valor e um status, que
 definirá se o ingresso já foi vendido. O construtor da classe deve receber como
 parâmetro apenas o valor do ingresso. Sobrescreva o método __str__ para que faça a
 impressão dos atributos da classe (código, status e valor). (1,0 ponto)
+
 A classe Camarote é subclasse de Ingresso e possui como atributo específico um valor
 adicional em relação ao ingresso de pista. Este valor adicional deve ser passado como
 parâmetro no construtor da classe. Sobrescreva o método __str__ para que faça a
 impressão dos atributos da classe (código, status e valor), atenção ao valor do ingresso
 de camarote que deve ser mostrado já com a soma do adicional.(1,0 ponto)
+
 A classe Show que possui como atributos: nome do Artista, Data do show, local do show
 e duas listas: uma dos ingressos de pista e outra dos ingressos Camarote que devem
 ser inicializadas vazias.(0,5 ponto) Defina um construtor que receba as informações do
@@ -28,25 +31,42 @@ compra.(1,0 ponto)
 relatorioVendas(), neste método deverá ser apresentado a lista de todos os
 ingressos vendidos até o presente momento, apresentando as informações do ingresso,
 e apresentando ao final o total arrecadado com a venda dos ingressos(pista e
-camarote). No cabeçalho do relatório deve aparecer os dados do show.(1,0 ponto)
-Utilize o arquivo bilheteria.py disponível em nossa sala de aula do Google
-Classroom para testar as classes.'''
+camarote). No cabeçalho do relatório deve aparecer os dados do show.(1,0 ponto)'''
 
+from datetime import date, datetime
 
 class Ingresso:
     def __init__(self, valor = 200.00) -> None:
         self.valor = valor
         self.status = ''
+        self.codigo = 0
     
-    @staticmethod
-    def codigo(cod):
-        return cod > 50
-
     def __str__(self) -> str:
-        return f'O valor do ingresso é {self.valor} e o status é {self.status}, {i.codigo}'
+        return f'O valor do ingresso é {self.valor} e o status é {self.status} e o código único é {self.codigo}'
+
+class Camarote(Ingresso):
+    def __init__(self, valor=200, adicional=100):
+        super().__init__(valor)
+        self.adicional = valor + adicional
+    
+    def __str__(self) -> str:
+        return f'O valor do ingresso é {self.adicional} e o status é {self.status} e o código único é {self.codigo}'
+
+class Show:
+    def __init__(self, artista, data, local) -> None:
+        self.artista = artista
+        self.data = datetime.date()
 
 
-i = Ingresso()
+
+
+
+
+
+
+
+
+i = Camarote()
 i.status = 'aoba'
-print(i.codigo(34))
+i.codigo = 34
 print(i)
