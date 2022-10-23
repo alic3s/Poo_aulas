1bffe933723676dedd6d535b97b2c4cb41646068
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-23 11:47:53 -0300

    Update Poo_curso_professor_aluno.py

--- Poo_curso_professor_aluno.py
+++ Poo_curso_professor_aluno.py
@@ -1,32 +1,57 @@
+from datetime import date
+from random import randint
+
 class Curso:
     def __init__(self, codigo, nome, descricao, coordenador):
         self.codigo = codigo
         self.nome = nome
         self.descricao = descricao
-        self.coordenador = coordenador
-        self.docentes = []
-        
-    def definir_coordenador(self, professor):
-        self.coordenador = professor
+        self._coordenador = None
+        self._docentes = []
     
-    def add_docente(self, professor):
+    @property
+    def coordenador(self):
+        return self._coordenador
+    
+    @coordenador.setter
+    def coordenador(self, professor):
+        self._coordenador = professor
+    
+    @property
+    def docentes(self):
+        return self._docentes
+
+    @docentes.setter
+    def docentes (self, professor):
         self.docentes.append(professor)
 
     def __str__ (self):
-        return f"[Código: {self.codigo}\nNome: {self.nome}\nDescrição: {self.descricao}\nCoordenador: {self.coordenador}\nDocentes: {self.docentes}]"
+        texto = f'Código: {0}\nNome: {1}\nDescrição{3}'.format(self.codigo, self.nome, self.descricao)
+        if self.coordenador is not None:
+            texto += '\nCoordenador: {}\n'.format(self.coordenador.nome)
+        textoDocentes = 'Docentes: \n'
+        if self._docentes is not None:
+            for elemento in self.docentes:
+                textoDocentes = textoDocentes + '\n' + elemento.nome
+        return texto + textoDocentes
 
 class Professor:
-    def __init__(self, matricula, nome_, cpf, admissao, email):
-        self.matricula = matricula
-        self.nome_ = nome_
+    def __init__(self, nome, cpf, dt_admissao, curso_lotacao, email = None):
+        self.matricula = __class__.gerarMatricula()
+        self.nome = nome
         self.cpf = cpf
-        self.admissao = admissao
+        self.admissao = dt_admissao
         self.email = email
-        self.lotacao = None
-    
-    def set_lotacao(self, curso: Curso):
-        self.lotacao = curso
+        self._lotacao = curso_lotacao
 
+    @property
+    def lotacao(self, curso):
+        return self._lotacao
+
+    @lotacao.setter
+    def lotacao(self, curso):
+        self._lotacao = curso
+        
     def __str__ (self):
         return f' [Matrícula: {self.matricula}, o nome: {self.nome_}, cpf: {self.cpf}, admissao: {self.admissao} email: {self.email}, lotação: {self.lotacao}]'
 
