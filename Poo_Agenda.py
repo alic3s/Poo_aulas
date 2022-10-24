13c611dd2e1ec47459ca9d67b1f3b7c671b7a1bf
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-24 20:17:51 -0300

    Update Poo_Agenda.py

--- Poo_Agenda.py
+++ Poo_Agenda.py
@@ -1,5 +1,18 @@
+class Telefone:
+    def __init__(self, ddd, numero, descricao):
+        self.ddd = ddd
+        self.numero = numero
+        self.descricao = descricao
+    
+    def alterar_numero(self, numero):
+        self.numero = numero
+
+    def __str__(self):
+        return '({0}) {1} --> {2}'.format(self.ddd, self.numero, self.descricao)
+
+
 class Contato:
-    def __init__(self, nome, email = None, telefone = None):
+    def __init__(self, nome, email, telefone: Telefone):
         self.nome = nome
         self.email = email
         self.telefone = [telefone]
@@ -7,8 +20,8 @@ class Contato:
     def alterar_email(self, email):
         self.email = email
     
-    def alterar_telefone(self, telefone):
-        if len(self.telefone) <3:
+    def alterar_telefone(self, telefone: Telefone):
+        if len(self.telefone) <= 3:
             self.telefone.append(telefone)
         else:
             print('Memória cheia')
@@ -17,80 +30,30 @@ class Contato:
         self.nome = nome
     
     def __str__(self):
-        texto = 'Nome: {}\nEmail: {}\nTelefones:\n'
+        texto = f'Nome: {self.nome}\nEmail: {self.email}\nTelefones:{self.telefone}\n'
         for item in self.telefone:
             if item != None:
-                texto+='({0}) {1}-{2}'.format(item.DDD, item.numero, item.descricao)
+                texto += str(item) + " "
         return texto
 
 
-class Telefone:
-    def __init__(self, DDD, numero, descricao):
-        self.DDD = DDD
-        self.numero = numero
-        self.descricao = descricao
-    
-    def alterar_numero(self, numero):
-        self.numero = numero
-
-    def __str__(self):
-        return '({0}) {1}-{2}'.format(self.DDD, self.numero, self.descricao)
-
-print('-' * 25 + '\n' + 'AGENDA'.center(25) + '\n' + '-' * 25)
-
-dicio = {'1)': 'Inserir Contato', '2)': 'Pesquisar Contatos', '3)': 'Alterar Contatos', '4)': 'Sair'}
-
-for k, v in dicio.items():
-    print(f'{(k)} {v:^5}')
-
 class Agenda:
-    def __init__(self):
-        self.contatos = []
+    def __init__(self, contatos: Contato):
+        self.contatos = [contatos]
 
-    def pesquisar_nome(self, nome):
+    def pesquisar_nome(self, nome) -> Contato:
         for contato in self.contatos:
             if contato.nome == nome:
                 return contato
         return None
         
-    def alterar_contato(self, nome):
-        self.contatos = ''
+    def alterar_contato(self, nm_antigo, nm_novo, email, telefone: Telefone):
+        antigo = self.pesquisar_nome(nm_antigo)
+        novo = Contato(nm_novo, email, telefone)
+        self.contatos[self.contatos.index(antigo)] = novo
         
-    def add_contato(self, contato):
-        self.contatos.append(contato)
-    
-
-print ("""
-    1.Inserir Contato
-    2.Pesquisar Contato
-    3.Alterar Contato
-    4.Sair
-    """)
-
-menu = True
-while menu:
-    menu = int(input('Escolha uma opção: '))
-if menu == 1:
-    contato1 = Contato()
-    agenda.add.contato
-elif menu == 2:
-    agenda.pesquisar_nome
-    print(nome.self.contato)
-elif menu == 3:
-    print ("""
-    3.1.Nome
-    3.2.Email
-    3.3.Telefone
-    """)
-    alterar = (float(input('Qual informação deseja alterar?: ')))
-    if alterar == 3.1:
-        contato.alterar_nome
-        print(self.nome)
-    elif alterar == 3.2:
-        contato.alterar_email
-        print(self.email)
-    elif alterar == 3.3:
-        contato.alterar_telefone
-        print(self.telefone)
-elif menu == 4:
-    break
+    def add_contato(self, contato: Contato):
+        if len(self.contatos) <= 10:
+            self.contatos.append(contato)
+        else:
+            print('Agenda cheia!')
