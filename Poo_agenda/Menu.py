07b10260dfa96242357d68d0749b92a39a39fa00
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-26 11:15:42 -0300

    Update Menu.py

--- Poo_agenda/Menu.py
+++ Poo_agenda/Menu.py
@@ -1,10 +1,4 @@
-'''print('-' * 25 + '\n' + 'AGENDA'.center(25) + '\n' + '-' * 25)
-
-dicio = {'1)': 'Inserir Contato', '2)': 'Pesquisar Contatos', '3)': 'Alterar Contatos', '4)': 'Sair'}
-
-for k, v in dicio.items():
-    print(f'{(k)} {v:^5}')
-
+from Contato import Telefone, Contato, Agenda
 
 print ("""
     1.Inserir Contato
@@ -16,32 +10,36 @@ print ("""
 menu = True
 while menu:
     menu = int(input('Escolha uma opção: '))
-if menu == 1:
-    contato1 = Contato()
-    agenda.add.contato
-elif menu == 2:
-    agenda.pesquisar_nome
-    print(nome.self.contato)
-elif menu == 3:
-    print ("""
-    3.1.Nome
-    3.2.Email
-    3.3.Telefone
-    """)
-    alterar = (float(input('Qual informação deseja alterar?: ')))
+    if menu == 1:
+        contato1 = Contato(str(input('Nome do Contato: ')), input('Email do Contato: '), int(input('Telefone do Contato: ')))
+        Agenda.add_contato((contato1))
+    elif menu == 2:
+        Agenda.pesquisar_nome()
+    if menu == 3:
+        print ("""
+        3.1.Nome
+        3.2.Email
+        3.3.Telefone
+        """)
+    alterar = Contato(float(input('Qual informação deseja alterar?: ')))
     if alterar == 3.1:
-        contato.alterar_nome
-        print(self.nome)
-    elif alterar == 3.2:
-        contato.alterar_email
-        print(self.email)
-    elif alterar == 3.3:
-        contato.alterar_telefone
-        print(self.telefone)
-elif menu == 4:
-    break'''
+        Contato.alterar_nome
+    if alterar == 3.2:
+        Contato.alterar_email
+    if alterar == 3.3:
+        Contato.alterar_telefone
+    else:
+        break
 
 
+'''print('-' * 25 + '\n' + 'AGENDA'.center(25) + '\n' + '-' * 25)
+
+dicio = {'1)': 'Inserir Contato', '2)': 'Pesquisar Contatos', '3)': 'Alterar Contatos', '4)': 'Sair'}
+
+for k, v in dicio.items():
+    print(f'{(k)} {v:^5}')
+    
+    
 from Contato import Contato, Telefone, Agenda
 
 t1 = Telefone(79, 998515346, 'Casa')
