c151b83233b7215a698debc354a2dbdd376376d2
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-11-01 18:29:45 -0300

    Update Poo_RevisaoProva.py

--- Poo_RevisaoProva.py
+++ Poo_RevisaoProva.py
@@ -29,7 +29,8 @@ class Conta:
     
     def depositar(self):
         deposito = float(input('Valor a ser depositado: '))
-        print(f'O saldo na conta atualizado com o depósito é: {self.saldo + deposito}')
+        self.saldo = self.saldo + deposito
+        print(f'O saldo na conta atualizado com o depósito é: {self.saldo}')
 
 class ContaPoupanca(Conta):
     def __init__(self, nm_cliente, n_conta, saldo, dia_rendimento):
@@ -37,7 +38,11 @@ class ContaPoupanca(Conta):
         self.dia_rendimento = int(dia_rendimento)
     
     def calcularNovoSaldo(self):
-
+        #nao sei como verificar se é ou não a data de aniversário do depósito
+        '''O rendimento, como já explicado, é de 0,5% por cento ao mês com a Taxa Selic em vigor. O rendimento é creditado em sua conta mensalmente,
+        no dia do aniversário do depósito. Por exemplo, se você fez um depósito de R$100,00 no dia 15 de algum mês,
+        os juros vão cair no dia 15 do mês seguinte.'''
+        #self.saldo = self.saldo * 0,5
 
 class ContaEspecial(Conta):
     def __init__(self, nm_cliente, n_conta, saldo, limite):
@@ -45,9 +50,14 @@ class ContaEspecial(Conta):
         self.limite = float(limite)
     
     def sacar(self):
-        
+        #to no caminho mas com certeza ta errado
+        sacar = float(input('Valor a ser sacado: '))
+        if sacar > self.limite:
+            self.saldo = -self.limite
+            print('Seu saldo atual é {}', self.saldo, 'e ao sacar passa a ser {}', (self.saldo - sacar))
+        else:
+            print(f'Seu saldo ao sacar é {self.saldo - sacar}')
 
-c = Conta('Alice', 123, 45.00)
+c = ContaEspecial('Alice', 123, 200.00, 400.00)
 
-c.depositar()
 c.sacar()
