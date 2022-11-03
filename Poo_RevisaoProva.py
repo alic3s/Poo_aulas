cf5f4018103d57021a50456fbc3fee6a1deba363
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-11-03 11:58:14 -0300

    Update Poo_RevisaoProva.py

--- Poo_RevisaoProva.py
+++ Poo_RevisaoProva.py
@@ -14,50 +14,96 @@ Depositar valor nas contas
 Mostrar novo saldo, a partir da taxa de rendimento, da conta poupança.
 Mostrar os dados da conta de um cliente.'''
 
+from datetime import date
+
 class Conta:
     def __init__(self, nm_cliente, n_conta, saldo):
         self.nm_cliente = nm_cliente
         self.n_conta = n_conta
-        self.saldo = saldo
+        self.saldo = float(saldo)
     
     def sacar(self):
-        valor = float(input('Valor a ser sacado: '))
+        valor = float(input('Quanto quer sacar? '))
         if valor > self.saldo:
             print('Saldo insuficiente')
         else:
-            print(f'O saldo atual é {self.saldo - valor}')
+            self.saldo = self.saldo - valor
+            print(f'\nAo sacar esse valor o saldo passa a ser R${self.saldo}')
     
     def depositar(self):
-        deposito = float(input('Valor a ser depositado: '))
+        deposito = float(input('Quanto quer depositar? '))
         self.saldo = self.saldo + deposito
-        print(f'O saldo na conta atualizado com o depósito é: {self.saldo}')
+        print(f'\nO saldo na conta atualizado com o depósito é R${self.saldo}\n')
+    
+    def imprimir(self):
+        print(f'\nTitular da Conta --> {self.nm_cliente}')
+        print(f'\nNúmero da Conta --> {self.n_conta}')
+        print(f'\nSaldo na Conta --> {self.saldo}')
 
 class ContaPoupanca(Conta):
-    def __init__(self, nm_cliente, n_conta, saldo, dia_rendimento):
+    def __init__(self, nm_cliente, n_conta, saldo, dt_rendimento):
         super().__init__(nm_cliente, n_conta, saldo)
-        self.dia_rendimento = int(dia_rendimento)
+        self.dt_rendimento = int(dt_rendimento)
+        self.dt_hj = date.today().day
     
     def calcularNovoSaldo(self):
-        #nao sei como verificar se é ou não a data de aniversário do depósito
-        '''O rendimento, como já explicado, é de 0,5% por cento ao mês com a Taxa Selic em vigor. O rendimento é creditado em sua conta mensalmente,
-        no dia do aniversário do depósito. Por exemplo, se você fez um depósito de R$100,00 no dia 15 de algum mês,
-        os juros vão cair no dia 15 do mês seguinte.'''
-        #self.saldo = self.saldo * 0,5
+        if self.dt_hj == self.dt_rendimento:
+            taxa = float(input('Valor da sua taxa de rendimento: '))
+            rendimento = self.saldo * (taxa / 100)
+            self.saldo = self.saldo + rendimento
+            print(f'\nAtualizamos sua conta com sua taxa de rendimento e seu saldo passou a ser R${self.saldo:.2f}'.format(self.saldo))
+        else:
+            print(f'\nO dia do seu rendimento é {self.dt_rendimento}, tente novamente em {self.dt_rendimento - self.dt_hj} dias')
+    
+    def depositar(self):
+        deposito = float(input('\nQuanto quer depositar? \n'))
+        self.saldo = self.saldo + deposito
+        print(f'\nO saldo na conta atualizado com o depósito é R${self.saldo:.2f}'.format(self.saldo))
+    
+    def imprimir(self):
+        print(f'Data do rendimento da Conta --> {self.dt_rendimento}')
+        return super().imprimir()
 
 class ContaEspecial(Conta):
     def __init__(self, nm_cliente, n_conta, saldo, limite):
         super().__init__(nm_cliente, n_conta, saldo)
         self.limite = float(limite)
+        self.limite2 = self.limite
     
     def sacar(self):
-        #to no caminho mas com certeza ta errado
-        sacar = float(input('Valor a ser sacado: '))
-        if sacar > self.limite:
-            self.saldo = -self.limite
-            print('Seu saldo atual é {}', self.saldo, 'e ao sacar passa a ser {}', (self.saldo - sacar))
+        saque = float(input('Quanto quer sacar? '))
+        if saque > self.saldo:
+            negativo = saque - self.saldo
+            if self.limite > negativo:
+                self.limite = self.limite - negativo
+                self.saldo = self.saldo - saque
+                print(f'Seu saldo atual é {self.saldo}, e seu limite {self.limite}')
         else:
-            print(f'Seu saldo ao sacar é {self.saldo - sacar}')
+            self.saldo = self.saldo - saque
+            print(f'\nAo sacar esse valor o saldo passa a ser R${self.saldo}')
+    
+    def depositar(self):
+        deposito = float(input('\nQuanto quer depositar? '))
+        self.saldo = self.saldo + deposito
+        self.limite = self.limite
+        print(f'\nO saldo na conta atualizado com o depósito é R${self.saldo}\n')
+    
+    def imprimir(self):
+        print(f'Limite da Conta --> {self.limite2}')
+        return super().imprimir()
 
-c = ContaEspecial('Alice', 123, 200.00, 400.00)
 
+c = Conta('Alice', 209, 100.00)
+c.depositar()
 c.sacar()
+c.imprimir()
+
+cp = ContaPoupanca('Rui', 708, 55.40, 3)
+cp.calcularNovoSaldo()
+cp.depositar()
+cp.imprimir()
+
+ce = ContaEspecial('Maria', 465, 25.89, 60)
+ce.sacar()
+ce.depositar()
+ce.imprimir()
