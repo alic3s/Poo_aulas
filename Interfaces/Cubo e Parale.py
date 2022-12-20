c20f7178c1374a95332e652356bf8fac1584ea70
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-12-19 21:50:32 -0300

    Update Cubo e Parale.py

--- Interfaces/Cubo e Parale.py
+++ Interfaces/Cubo e Parale.py
@@ -1,12 +1,12 @@
 from poliedro import Poliedro
 
 class Cubo(Poliedro):
-    def __init__(self, qtdVertice, qtdAresta, qtdFace, lado1, lado2, altura, lado):
-        super().__init__(qtdVertice, qtdAresta, qtdFace, lado1, lado2, altura)
-        self.lado = lado
-
+    def __init__(self, qtdVertice, qtdAresta, qtdFace, lado1):
+        Poliedro.__init__(qtdVertice, qtdAresta, qtdFace, lado1, lado1, lado1)
+        
     def calculaArea(self):
         return 2 * (self.lado + self.lado + self.lado)
+        6 * math.pow(self.lado1, 2)
     
     def calculaVolume(self):
         return 3 * self.lado
