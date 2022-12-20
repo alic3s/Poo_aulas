ee9934438c389206f5feb19eb0c52f4759e706cd
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-12-19 21:38:42 -0300

    Create iFormaGeometrica.py

---
+++ Interfaces/iFormaGeometrica.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+class iFormaGeometrica(ABC):
+    'nao sei o que eu to fazendo'
+    @abstractmethod
+    def calculaArea(self):
+        pass
+
+    @abstractmethod
+    def calculaVolume(self):
+        pass
