8c26277557664c4dc33901d97df02eba48410d2e
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-12-19 21:47:05 -0300

    Update Cilindro.py

--- Interfaces/Cilindro.py
+++ Interfaces/Cilindro.py
@@ -1,3 +1,5 @@
+import math
+
 class Cilindro:
     def __init__(self, raio, altura) -> None:
         self.raio = raio
@@ -5,9 +7,11 @@ class Cilindro:
     
     def calculaArea(self):
         return {2 * 3,14 * self.raio(self.altura + self.raio)}
+        2 * math * ....
         
     def calculaVolume(self): 
         return {3,14 * self.raio * self.raio * self.altura}
+        math.pi.pow...
     
     def __str__(self):
         return f'Area: {self.calculaArea}\nVolume: {self.calculaVolume}'
