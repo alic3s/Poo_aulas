544f75e6c91f940fff850743dc024e0d5c421bd1
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-12-27 16:56:51 -0300

    Create Salario.py

---
+++ Interfaces/exe2/Salario.py
@@ -0,0 +1,9 @@
+from pagavel import Pagavel
+
+class ControlePagamento(Pagavel):
+    def __init__(self, salario = 1100.00, hora = 8.00):
+        self.salario = salario
+        self.hora = hora
+
+
+#class SistemaPagamentoTeste:
