144bb37f96815fddfd06c6146cef816f82c5110a
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-12-27 17:04:03 -0300

    Update Empregado.py

--- Interfaces/exe2/Empregado.py
+++ Interfaces/exe2/Empregado.py
@@ -1,5 +1,12 @@
+from salario import ControlePagamento
+
 class Empregado:
     def __init__(self, nome, sobrenome, numident):
         self.nome = str(nome)
         self.sobrenome = str(sobrenome)
         self.numident = numident
+    
+
+class Assalariado(Empregado):
+    def __init__(self, nome, sobrenome, numident):
+        super().__init__(nome, sobrenome, numident)
