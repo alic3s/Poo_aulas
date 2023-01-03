774b8ad15e2a0a1c7119a5de35e5a752f9f2c0ff
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2023-01-03 16:29:17 -0300

    Update Empregado.py

--- Interfaces/exe2/Empregado.py
+++ Interfaces/exe2/Empregado.py
@@ -1,12 +1,27 @@
 from salario import ControlePagamento
 
-class Empregado:
-    def __init__(self, nome, sobrenome, numident):
-        self.nome = str(nome)
-        self.sobrenome = str(sobrenome)
+class Empregado(ControlePagamento):
+    def __init__(self, salario=1100, hora=8, nome=str, sobrenome=str, numident=float):
+        super().__init__(salario, hora)
+        self.nome = nome
+        self.sobrenome = sobrenome
         self.numident = numident
     
 
 class Assalariado(Empregado):
-    def __init__(self, nome, sobrenome, numident):
-        super().__init__(nome, sobrenome, numident)
+    def __init__(self, salario=1100, hora=8, nome=str, sobrenome=str, numident=float, horasem=int, horaextra=None):
+        super().__init__(salario, hora, nome, sobrenome, numident, horasem, horaextra)
+        self.horasem = horasem
+        self.horaextra = horaextra 
+
+        if self.horasem == 40:
+            print('Salário fixo de ', self.salario)
+        elif self.horasem > 40:
+            print('Salário fixo acrescido das horas extras ', self.salario + (self.hora * self.horaextra))
+
+
+class Terceirizado(Empregado):
+    def __init__(self, salario=1100, hora=8, nome=str, sobrenome=str, numident=float, horasem=int):
+        super().__init__(salario, hora, nome, sobrenome, numident, horasem)
+        self.horasem = horasem
+        print('Salário por hora trabalhada ', self.horasem * self.hora)
