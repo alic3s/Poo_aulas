3d56bc2e86341389b5d778de7d6637a6c05810bf
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2023-01-16 21:35:30 -0300

    Create Revisão.py

---
+++ Tratamento_Exceção/Revisão.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+class iAutenticavel(ABC):
+    @abstractmethod
+    def autenticar(self):
+        pass
+
+class SaldoExcecaoError(Exception):
+    pass
+
+def validarSaque(self, saque):
+    if saque > self.saldo:
+        raise 
+
+class Conta:
+    def __init__(self, agencia=str, numConta=int, saldo=float, senha=str):
+        self.agencia = agencia
+        self.numConta = numConta
+        self.saldo = saldo
+        self.senha = senha
+    
+    def depositar(self, valor) -> float:
+        try:
+            self.saldo += valor
+        except TypeError:
+            print('Erro: Tipo Inválido!')
+       
+
+    def sacar(self) -> float:
+        try:
+            s = input(float('Valor a ser sacado: '))
+            validarSaque(s)
+        except SaldoExcecaoError():
